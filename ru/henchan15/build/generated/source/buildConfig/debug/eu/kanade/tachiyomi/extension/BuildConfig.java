011d8b0230188f599f9777e9477e35ec97b0498e
ForsunJay <forsunJay@users.noreply.github.com>
2020-09-17 12:51:52 +0300

    Initial commit

---
+++ ru/henchan15/build/generated/source/buildConfig/debug/eu/kanade/tachiyomi/extension/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eu.kanade.tachiyomi.extension;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "eu.kanade.tachiyomi.extension.ru.henchan15";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 19;
+  public static final String VERSION_NAME = "6.19";
+}
